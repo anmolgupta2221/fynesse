992544c4483459eb2a983619ae2a449896217ae4
anmolgupta2221 <105435853+anmolgupta2221@users.noreply.github.com>
2023-12-04 18:24:37 +0000

    Update access.py
    credentials

--- fynesse/access.py
+++ fynesse/access.py
@@ -9,7 +9,18 @@ import sqlite"""
 
 # This file accesses the data
 
+import yaml
+
 """Place commands in this file to access the data electronically. Don't remove any missing values, or deal with outliers. Make sure you have legalities correct, both intellectual property and personal data privacy rights. Beyond the legal side also think about the ethical issues around this data. """
+
+# Code for requesting and storing credentials (username, password) of the RDS
+def write_credentials(username, password):
+    with open("credentials.yaml", "w") as file:
+        credentials_dict = {'username': username,
+                            'password': password}
+        yaml.dump(credentials_dict, file)
+
+
 
 def data():
     """Read the data from the web or local file, returning structured format such as a data frame"""
