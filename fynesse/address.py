f4a57df4f50e4d6fabc66ba14179a83fb0c39c52
anmolgupta2221 <105435853+anmolgupta2221@users.noreply.github.com>
2023-12-06 23:22:50 +0000

    refactoring

--- fynesse/address.py
+++ fynesse/address.py
@@ -10,11 +10,15 @@ import sklearn.tree as tree
 import GPy
 import torch
 import tensorflow as tf
-
 # Or if it's a statistical analysis
 import scipy.stats"""
 
 """Address a particular question that arises from the data"""
+
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import math
 
 # Helper function for applying indicator functions to features
 def indicator_function(row, feature, column_name):
@@ -42,4 +46,123 @@ def feature_picker(df,column):
     return pd.DataFrame.empty
   else:
     return df[df[column].notna()]
+
+# Fit the model based on y data and design matrix 
+def fit_model(y, design):
+  m_linear_basis = sm.OLS(y,design)
+  results_basis = m_linear_basis.fit()
+  results_basis_0 = m_linear_basis.fit_regularized(alpha=0.10,L1_wt=0.0)
+  results_basis_1 = m_linear_basis.fit_regularized(alpha=0.10,L1_wt=0.3)
+  results_basis_2 = m_linear_basis.fit_regularized(alpha=0.10,L1_wt=0.6)
+  results_basis_3 = m_linear_basis.fit_regularized(alpha=0.10,L1_wt=1.0)
+  return (results_basis, results_basis_0, results_basis_1, results_basis_2, results_basis_3)
+
+# Make predictions based on results basis and design matrix
+def predict_model(design, results_basis, results_basis_0, results_basis_1, results_basis_2, results_basis_3):
+  y_pred = results_basis.predict(design)
+  y_pred_0 = results_basis_0.predict(design)
+  y_pred_1 = results_basis_1.predict(design)
+  y_pred_2 = results_basis_2.predict(design)
+  y_pred_3 = results_basis_3.predict(design) 
+  return(y_pred, y_pred_0, y_pred_1, y_pred_2, y_pred_3)
+
+# Extract relevant dataframes from the join 
+def extract_relevant_df(latitude, longitude, date, property_box_size, date_range):
+  all_years_properties = fynesse.access.joinq(conn, latitude, longitude, property_box_size)
+  input_date = datetime.strptime(date, "%B %Y")
+  relevant_years_properties = all_years_properties[(all_years_properties['date_of_transfer'] >= (input_date - timedelta(days=365*date_range))) & (all_years_properties['date_of_transfer'] <= (input_date + timedelta(days=365*date_range)))]
+  return relevant_years_properties 
+
+# Setup all the features from the property_prices database using the indicator functions
+def property_prices_features(df):
+  detatched = indicator('D', 'property_type', df)
+  semi_detatched = indicator('S', 'property_type', df)
+  flat = indicator('F', 'property_type', df)
+  terraced = indicator('T', 'property_type', df)
+  other = indicator('O', 'property_type', df)
+  new_build = indicator('Y', 'new_build_flag', df)
+  tenure_type_f = indicator('F', 'tenure_type', df)
+  tenure_type_l = indicator('L', 'tenure_type', df)
+  return (detatched, semi_detatched, flat, terraced, other, new_build, tenure_type_f, tenure_type_l)
+
+# Function to count matching records in pois for each row in df
+def count_matching_pois(row, pois, threshold):
+    lat = row['latitude']
+    lon = row['longitude']
+    matching_pois = pois[
+        (pois['latitude'] >= lat - threshold) & (pois['latitude'] <= lat + threshold) &
+        (pois['longitude'] >= lon - threshold) & (pois['longitude'] <= lon + threshold)
+    ]
+    return len(matching_pois)
+
+#Function to get the centroid of each geometry and add to pois
+def adjust_pois(pois):
+  lat,longit = get_centroid(pois)
+  pois = fynesse.assess.labelled(pois)
+  pois['latitude'] = lat
+  pois['longitude'] = longit
+  return pois
+
+#Function to calculate R squared value of prediction
+def r_squared_calc(prediction):
+  # Calculate the mean of the dependent variable
+  y_mean = np.mean(y)
+
+  # Calculate total sum of squares
+  total_sum_of_squares = np.sum((y - y_mean)**2)
+
+  # Calculate residual sum of squares
+  residual_sum_of_squares = np.sum((y - prediction)**2)
+
+  # Calculate R-squared
+  r_squared = 1 - (residual_sum_of_squares / total_sum_of_squares)
+  return r_squared
+
+#Function to return closest distance to specified dataframe containing points of interest
+def closest_pois_distance(row, pois):
+    lat = row['latitude']
+    lon = row['longitude']
+    
+    # Calculate Euclidean distances to all pois
+    distances = cdist([(lat, lon)], pois[['latitude', 'longitude']], metric='euclidean')[0]
+    
+    # Find the index of the closest pois
+    closest_index = np.argmin(distances)
+    
+    # Return the distance to the closest pois
+    return distances[closest_index]
+
+#Function to return sigmoid (kinda) of feature
+def sigmoidish(x):
+    return ((1 / (1 + np.exp(-0.3 * x))) - 0.5)
+
+
+def osm_feature_count(df, amenities, schools, healthcare, leisure, public_transport, threshold):
+  amenity_count = np.array(df.apply(count_matching_pois, pois=amenities, threshold=threshold, axis=1)) 
+  school_count = np.array(df.apply(count_matching_pois, pois=schools, threshold = threshold, axis=1))
+  healthcare_count = np.array(df.apply(count_matching_pois, pois=healthcare, threshold = threshold, axis=1))
+  leisure_count = np.array(df.apply(count_matching_pois, pois=leisure, threshold = threshold, axis=1))
+  p_trans_count = np.array(df.apply(count_matching_pois, pois=public_transport, threshold = threshold, axis=1))
+  return(amenity_count, school_count, healthcare_count, leisure_count, p_trans_count)
+
+def fit_and_predict(latitude, longitude, date, property_box_size = 0.01, date_range = 1, osm_box_size = 0.02, feature_decider = 'variation 1', threshold = 0.01):
+  df = extract_relevant_df(latitude, longitude, date, property_box_size, date_range)
+  detatched, semi_detatched, flat, terraced, other, new_build, tenure_type_f, tenure_type_l = property_prices_features(df)
+  pois, _,_,_,_ = fynesse.assess.get_geometries(latitude, longitude, osm_box_size, {'amenity':True, 'healthcare':True, 'leisure':True, 'public_transport':True})
+  pois = adjust_pois(pois)
+  amenities, schools, healthcare, leisure, public_transport = relevant_pois(pois)
+  amenity_count, school_count, healthcare_count, leisure_count, p_trans_count = osm_feature_count(df, amenities, schools, healthcare, leisure, public_transport, threshold)
+  if (feature_decider == 'count'):
+    amenity_feature, school_feature, healthcare_feature, leisure_feature, p_trans_feature = amenity_count, school_count, healthcare_count, leisure_count, p_trans_count
+  elif (feature_decider == 'variation 1'):
+    amenity_feature, school_feature, healthcare_feature, leisure_feature, p_trans_feature = np.sqrt(amenity_count), sigmoidish(school_count), np.array(df.apply(closest_pois_distance, pois=pois, df=df, axis=1)), leisure_count, np.array(df.apply(count_matching_pois, pois=public_transport, threshold = threshold, axis=1))
+  design = np.concatenate((detatched.reshape(-1,1), semi_detatched.reshape(-1,1), terraced.reshape(-1,1),
+                           flat.reshape(-1,1), other.reshape(-1,1), new_build.reshape(-1,1), tenure_type_f.reshape(-1,1), 
+                           tenure_type_l.reshape(-1,1), amenity_feature.reshape(-1,1), school_feature.reshape(-1,1), healthcare_feature.reshape(-1,1),
+                           p_trans_feature.reshape(-1,1), leisure_feature.reshape(-1,1)),axis=1)
+  y = np.array(df['price']).astype(float)
+  y += 0.05*np.random.randn(len(df))
+  results_basis, results_basis_0, results_basis_1, results_basis_2, results_basis_3 = fit_model(y, design)
+  y_pred, y_pred_0, y_pred_1, y_pred_2, y_pred_3 = predict_model(design, results_basis, results_basis_0, results_basis_1, results_basis_2, results_basis_3)
+  return (results_basis, results_basis_0, results_basis_1, results_basis_2, results_basis_3, y_pred, y_pred_0, y_pred_1, y_pred_2, y_pred_3)
 
